5065f866242aa5ecc2207cde5ab912654a664437
giantecca <giantecca@gmail.com>
2024-04-13 01:39:25 +0200

    hello world

---
+++ 00_helloworld.py
@@ -0,0 +1,23 @@
+# Esto es un comentario:
+# Nuestro Hola Mundo en Python
+print("Hola Python")
+print('Hola Python')
+
+'''
+Este es un
+comentario 
+en varias líneas
+'''
+
+"""
+Este también es un
+comentario
+en varias líneas
+"""
+
+# Consultar el tipo de dato
+print(type("Hola Python")) # Tipo 'str'
+print(type(5)) # Tipo 'int'
+print(type(1.5)) # Tipo 'float'
+print(type(1 + 1j)) # Tipo 'complex'
+print(type(True)) # Tipo 'bool'
