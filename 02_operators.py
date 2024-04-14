f26647bde9bf2f6d6b677e21f65191de0ef1cd1d
giantecca <giantecca@gmail.com>
2024-04-14 02:15:44 +0200

    operators

---
+++ 02_operators.py
@@ -0,0 +1,33 @@
+# Operadores aritméticos
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(10 % 2)
+print(10 // 3) # Aproxima la división
+print(2 ** 3) # Exponente
+print(2 ** 3 + 3 - 7 / 1 // 4)
+
+print("Hola" + "Python")
+print("Hola " + "Python")
+print("Hola " * 5)
+
+# Operadores comparativos
+print("---")
+
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(3 <= 4)
+print(3 == 4)
+print(3 != 4)
+
+# Operadores Lógicos
+print("---")
+
+print(3 > 4 and "Hola" > "Python")
+print(3 > 4 or "Hola" > "Python")
+print(3 < 4 and "Hola" < "Python")
+print(3 < 4 or "Hola" < "Python")
+print(not(3 > 4))
