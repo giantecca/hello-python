361d6ec0f69d5e0e9f2bc00d77cae07a8498629f
giantecca <giantecca@gmail.com>
2024-04-24 23:40:09 +0200

    modules

---
+++ 13_modules.py
@@ -0,0 +1,10 @@
+# Módulos
+
+# import my_module # No podemos importar ficheros que empicen por números. Por eso se ha creado el ficher my_module
+from my_module import sumValue, printValue # importa una operación concreta del fichero my_module
+
+# my_module.sumValue(5, 3, 1)
+# my_module.printValue('Hola Python!')
+
+sumValue(5, 3, 1)
+printValue('Hola Python!')
