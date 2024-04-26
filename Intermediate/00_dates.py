09863d98db893818139522296ca0435f49091a1b
giantecca <giantecca@gmail.com>
2024-04-26 12:29:58 +0200

    dates

---
+++ Intermediate/00_dates.py
@@ -0,0 +1,39 @@
+# Dates
+
+from datetime import datetime # Módulo del sistema relacionado con fechas
+
+now = datetime.now() # now() es una función que tenemos dentro de datetime
+
+def print_date(date):
+    print(date.year)
+    print(date.month)
+    print(date.day)
+    print(date.hour)
+    print(date.minute)
+    print(date.second)
+    print(date.timestamp())
+
+print_date(now)
+
+year_2023 = datetime(2023, 1, 1)
+
+print_date(year_2023)
+
+from datetime import time
+
+current_time = time(11, 32, 0)
+
+print(current_time.hour)
+print(current_time.minute)
+print(current_time.second)
+
+from datetime import date
+
+current_date = date.today()
+
+print(current_date.year)
+print(current_date.month)
+print(current_date.day)
+
+
+
